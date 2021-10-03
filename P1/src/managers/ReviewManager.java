793569b218245abc0d5f3d31aee169e3a5aaea2c
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-10-03 18:18:12 +0200

    Done Managers

--- P1/src/managers/ReviewManager.java
+++ P1/src/managers/ReviewManager.java
@@ -1,8 +1,13 @@
 package managers;
 
+import java.util.ArrayList;
+import reviews.Review;
+
 public class ReviewManager {
 	private static ReviewManager instance = null;
-
+	
+	private ArrayList<Review> reviews = new ArrayList<Review>();
+	
 	private ReviewManager(){
 		
 	}
@@ -12,5 +17,36 @@ public class ReviewManager {
 			instance = new ReviewManager();
 		}
 		return instance;
+	}
+
+	/**
+	 * Consulta todas las críticas disponibles
+	 * @return the reviews
+	 */
+	public ArrayList<Review> getReviews() {
+		return reviews;
+	}
+
+	/**
+	 * @param reviews the reviews to set
+	 */
+	public void setReviews(ArrayList<Review> reviews) {
+		this.reviews = reviews;
+	}
+	
+	public boolean registerReview(){
+		return true;
+	}
+	
+	public boolean deleteReview(){
+		return true;
+	}
+	
+	public boolean voteReview(){
+		return true;
+	}
+	
+	public Review searchReview(){
+		return null;	
 	}
 }
--- P1/src/managers/UserManager.java
+++ P1/src/managers/UserManager.java
@@ -1,9 +1,15 @@
 package managers;
 
+import java.util.ArrayList;
+
+import users.User;
+
 public class UserManager {
 
 	private static UserManager instance = null;
 
+	private ArrayList<User> users = new ArrayList<User>();
+	
 	private UserManager(){
 		
 	}
@@ -13,5 +19,35 @@ public class UserManager {
 			instance = new UserManager();
 		}
 		return instance;
+	}
+
+	/**
+	 * @return the users
+	 */
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+
+	/**
+	 * @param users the users to set
+	 */
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
+	}
+	
+	public boolean registerUser(){
+		return true;
+	}
+	
+	public boolean deleteUser(){
+		return true;
+	}
+	
+	public boolean modifyUser(){
+		return true;
+	}
+	
+	public User searchUser(){
+		return null;	
 	}
 }
