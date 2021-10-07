8df71ea456ce54ad79a78a233b2cdadbcae6b32f
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-10-07 11:56:29 +0200

    Starting E2

--- P1/src/programs/E2.java
+++ P1/src/programs/E2.java
@@ -1,7 +1,10 @@
 package programs;
 
+import java.io.IOException;
+import managers.UserManager;
 import storage.FileStorage;
 import storage.IRepository;
+import utilities.SystemFunctions;
 
 /**
  * Programa principal del ejercicio 2
@@ -14,7 +17,116 @@ import storage.IRepository;
 
 public class E2 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     IRepository entityRepository = new FileStorage();
+    UserManager userManager = UserManager.getInstance();
+
+    entityRepository.loadSystem();
+
+    int choice;
+
+    choice = SystemFunctions.printE1LoginScreen();
+
+    while (choice != 0) {
+      if (choice == 1) {
+        SystemFunctions.registerUser("Spectator");
+      }
+
+      if (choice == 2) {
+        if (SystemFunctions.loginUser()) {
+          if (userManager.getActiveUser().getType().equals("Spectator")) {
+            choice = SystemFunctions.printE1SpectatorMenuScreen();
+            while (choice != 0) {
+              if (choice == 1) {
+                SystemFunctions.registerReviewE1();
+              }
+              if (choice == 2) {
+                SystemFunctions.listReviews();
+              }
+              if (choice == 3) {
+                if (SystemFunctions.deleteReview()) {
+                  System.out.println("Borrado correcto");
+                } else {
+                  System.out.println("Error en el borrado");
+                }
+              }
+              if (choice == 4) {
+                if (SystemFunctions.voteReview()) {
+                  System.out.println("Valoración correcta");
+                } else {
+                  System.out.println("Error en la valoración");
+                }
+              }
+              if (choice == 5) {
+                SystemFunctions.searchUsersReview();
+              }
+              choice = SystemFunctions.printE1SpectatorMenuScreen();
+            }
+          }
+          if (userManager.getActiveUser().getType().equals("Admin")) {
+            choice = SystemFunctions.printE1AdminMenuScreen();
+            while (choice != 0) {
+              if (choice == 1) {
+                choice = SystemFunctions.printE1SpectatorMenuScreen();
+                while (choice != 0) {
+                  if (choice == 1) {
+                    SystemFunctions.registerReviewE1();
+                  }
+                  if (choice == 2) {
+                    SystemFunctions.listReviews();
+                  }
+                  if (choice == 3) {
+                    if (SystemFunctions.deleteReview()) {
+                      System.out.println("Borrado correcto");
+                    } else {
+                      System.out.println("Error en el borrado");
+                    }
+                  }
+                  if (choice == 4) {
+                    if (SystemFunctions.voteReview()) {
+                      System.out.println("Valoración correcta");
+                    } else {
+                      System.out.println("Error en la valoración");
+                    }
+                  }
+                  if (choice == 5) {
+                    SystemFunctions.searchUsersReview();
+                  }
+                  choice = SystemFunctions.printE1SpectatorMenuScreen();
+                }
+              }
+              if (choice == 2) {
+                choice = SystemFunctions.printE1AdminUsersScreen();
+                while (choice != 0) {
+                  if (choice == 1) {
+                    SystemFunctions.registerAdmin();
+                  }
+                  if (choice == 2) {
+                    if (SystemFunctions.deleteUser()) {
+                      System.out.println("Borrado correcto");
+                    } else {
+                      System.out.println("Error en el borrado");
+                    }
+                  }
+                  if (choice == 3) {
+                    SystemFunctions.searchUser();
+                  }
+                  if (choice == 4) {
+                    SystemFunctions.modifyUser();
+                  }
+                  choice = SystemFunctions.printE1AdminUsersScreen();
+                }
+              }
+              choice = SystemFunctions.printE1AdminMenuScreen();
+            }
+          }
+        } else {
+          System.out.println("Error de autentificación");
+        }
+      }
+      choice = SystemFunctions.printE1LoginScreen();
+    }
+    entityRepository.saveSystem();
+    SystemFunctions.printE1ExitScreen();
   }
 }
