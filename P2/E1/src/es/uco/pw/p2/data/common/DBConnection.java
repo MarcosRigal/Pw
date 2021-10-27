df5c989665c4d5bc043866054b190bc29076b9e2
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-10-27 21:30:04 +0200

    revert: :bug: Triying to hide bdd password

--- P2/E1/src/es/uco/pw/p2/data/common/DBConnection.java
+++ P2/E1/src/es/uco/pw/p2/data/common/DBConnection.java
@@ -12,44 +12,43 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-	protected Connection connection = null;
+  protected Connection connection = null;
 
-	// Important: This configuration is hard-coded here for illustrative purposes only
-	
-	protected String url = "jdbc:mysql://oraclepr.uco.es:3306/i92rigam";
+  // Important: This configuration is hard-coded here for illustrative purposes only
 
-	protected String user = "i92rigam";
+  protected String url = "jdbc:mysql://oraclepr.uco.es:3306/i92rigam";
 
-	protected String password = "PW2122";
+  protected String user = "i92rigam";
 
-	public Connection getConnection(){
+  protected String password = "PW2122";
 
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			this.connection = (Connection) DriverManager.getConnection(url, user, password);
-			System.out.println("Database connection successfully opened!");
-		} 
-		catch (SQLException e) {
-			System.err.println("Connection to MySQL has failed!");
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			System.err.println("JDBC Driver not found.");
-			e.printStackTrace();
-		}
-		return this.connection;
-	}
+  public Connection getConnection() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      this.connection =
+        (Connection) DriverManager.getConnection(url, user, password);
+      System.out.println("Database connection successfully opened!");
+    } catch (SQLException e) {
+      System.err.println("Connection to MySQL has failed!");
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      System.err.println("JDBC Driver not found.");
+      e.printStackTrace();
+    }
+    return this.connection;
+  }
 
-	// We can include here other methods to encapsulate CRUD commands...
+  // We can include here other methods to encapsulate CRUD commands...
 
-	public void closeConnection() {
-		try {
-			if(this.connection != null && !this.connection.isClosed()) {
-				this.connection.close();
-				System.out.println("Database connection successfully closed!");
-			}
-		} catch (SQLException e) {
-			System.err.println("Error while trying to close the connection.");
-			e.printStackTrace();
-		}
-	}
+  public void closeConnection() {
+    try {
+      if (this.connection != null && !this.connection.isClosed()) {
+        this.connection.close();
+        System.out.println("Database connection successfully closed!");
+      }
+    } catch (SQLException e) {
+      System.err.println("Error while trying to close the connection.");
+      e.printStackTrace();
+    }
+  }
 }
